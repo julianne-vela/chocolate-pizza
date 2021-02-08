9563f0aeb3ab38150337c2d6928663bdab5e0e89
Julianne Vela <cjvela@outlook.com>
2021-02-08 14:50:32 -0800

    started recipe instructions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './header.js';
 import MainArticle from './image.js';
+import RecipeSteps from './recipe-instructions.js';
 
 export default class App extends React.Component {
   render() {
@@ -8,6 +9,7 @@ export default class App extends React.Component {
       <>
         <Header />
         <MainArticle />
+        <RecipeSteps />
       </>
     );
   }
---
+++ src/recipe-instructions.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class RecipeSteps extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>
+
+                </p>
+                <p>
+
+                </p>
+                <p>
+
+                </p>
+            </div>
+        )
+    }
+}
