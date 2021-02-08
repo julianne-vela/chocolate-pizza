2b0357da8b1567a053d85a181890a3257d666d4d
Julianne Vela <cjvela@outlook.com>
2021-02-08 14:44:44 -0800

    added main article section

--- src/header.js
+++ src/header.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import logo from '../public/logo.png';
-import facebook from '../public/fb-icon.png';
-import twitter from '../public/twit-icon.png';
-import google from '../public/gp-icon.png';
-import instagram from '../public/insta-icon.png';
-import flickr from '../public/flic-icon.png';
-import pinterest from '../public/pint-icon.png';
-import rss from '../public/rss-icon.png';
-import mail from '../public/mail-icon.png';
+import logo from './assets/logo.png';
+import facebook from './assets/fb-icon.png';
+import twitter from './assets/twit-icon.png';
+import google from './assets/gp-icon.png';
+import instagram from './assets/insta-icon.png';
+import flickr from './assets/flic-icon.png';
+import pinterest from './assets/pint-icon.png';
+import rss from './assets/rss-icon.png';
+import mail from './assets/mail-icon.png';
 
 
 export default class Header extends React.Component {
-    render () {
+    render() {
         return (
             <div className="header">
-                <img alt="fork and knife logo" src={logo}/>
+                <img alt="fork and knife logo" src={logo} />
                 <h1>Delicious</h1>
                 <h2>THE BEST FOOD BLOG ON THE WEB.</h2>
                 <section>
-                    <img alt="facebook logo" src={facebook}/>
-                    <img alt="twitter logo" src={twitter}/>
-                    <img alt="google+ logo" src={google}/>
-                    <img alt="instagram logo" src={instagram}/>
-                    <img alt="flickr logo" src={flickr}/>
-                    <img alt="pinterest" src={pinterest}/>
-                    <img alt="rss" src={rss}/>
-                    <img alt="mail logo" src={mail}/>
+                    <img alt="facebook logo" src={facebook} />
+                    <img alt="twitter logo" src={twitter} />
+                    <img alt="google+ logo" src={google} />
+                    <img alt="instagram logo" src={instagram} />
+                    <img alt="flickr logo" src={flickr} />
+                    <img alt="pinterest" src={pinterest} />
+                    <img alt="rss" src={rss} />
+                    <img alt="mail logo" src={mail} />
                 </section>
             </div>
         )
