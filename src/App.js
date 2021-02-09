e7e4b93511e9ac5383d3dae0edb318f9fa4e63bf
Julianne Vela <cjvela@outlook.com>
2021-02-09 09:13:46 -0800

    corrected IngredientList component

--- src/App.js
+++ src/App.js
@@ -2,38 +2,19 @@ import React from 'react';
 import Header from './header.js';
 import MainArticle from './image.js';
 import RecipeSteps from './recipe-instructions.js';
-import { Ingredients } from './data.js';
 import IngredientList from './ingredient-list.js';
 import Footer from './footer.js';
 // import './App.module.css';
 
-class IngredientItem extends React.Component {
-  render() {
-    return <div>
-      <input type='checkbox' />
-      <p>
-        {this.props.ingredient.amount} {this.props.ingredient.unit} {this.props.ingredient.name}
-      </p>
-    </div>
-  }
-}
-
 export default class App extends React.Component {
   render() {
-    const ingredientsList = Ingredients.map(
-      ingredientItem =>
-        <IngredientItem ingredient={
-          ingredientItem
-        } />)
 
     return (
       <>
         <Header />
         <MainArticle />
         <RecipeSteps />
-        <div style={{
-          backgroundImage: 'url(/list-bg.png)'
-        }}>{ingredientsList}</div>
+        <IngredientList />
         <Footer />
       </>
     );
--- src/ingredient-list.js
+++ src/ingredient-list.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Ingredients } from './data.js';
 
+class IngredientItem extends React.Component {
+    render() {
+        return <div>
+            <input type='checkbox' />
+            <p>
+                {this.props.ingredient.amount} {this.props.ingredient.unit} {this.props.ingredient.name}
+            </p>
+        </div>
+    }
+}
 
 export default class IngredientList extends React.Component {
     render() {
+        const ingredientsList = Ingredients.map(
+            ingredientItem =>
+                <IngredientItem ingredient={
+                    ingredientItem
+                } />)
         return (
-            <div className='ingredientList' >
-
+            <div>
+                <div style={{
+                    backgroundImage: 'url(/list-bg.png)'
+                }}>{ingredientsList}</div>
             </div>
         )
     }
